ab4eb608620cc5c940ce43a9cd9c46f386804230
paranjaydubey <paranjaydubey19@gmail.com>
2024-07-02 09:50:23 +0530

    routes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,8 +9,24 @@ app.use(cors({
     credentials:true
 }))
 app.use(express.json({limit:"17kb"}))
-app.use(express.urlencoded({extended: true})) //limit: "17kb"
+app.use(express.urlencoded({extended: true, limit: "17kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//import routes
+import applicationRouter from "./routes/application.routes.js"
+import userRouter from "./routes/user.routes.js"
+import eventRouter from "./routes/event.routes.js"
+import eventUserRoute from "./routes/event-user.routes.js"
+import authRouter from "./routes/auth.routes.js"
+
+//routes declaration
+app.use('/events', eventRouter)
+app.use('/users', userRouter)
+app.use('/auth', eventUserRoute)
+app.use('/applications', applicationRouter)
+app.use('/eventusers', authRouter)
+
+//e.g.http://localhost:4000/api/v1/users/register
 
 export {app}
